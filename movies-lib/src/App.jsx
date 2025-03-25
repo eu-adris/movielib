248eca78b80229ba547d2499451dbe9f808c98dd
adriano <noturnoabs@hotmail.com>
2025-03-25 18:45:06 -0300

    adicionado a API, cores de undo e components

--- movies-lib/src/App.jsx
+++ movies-lib/src/App.jsx
@@ -1,19 +1,13 @@
 import './App.css'
-import { Link, Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
+import Navbar from './components/Navbar'
 
 function App() {
 
 
   return (
     <div className='App'>
-      <nav id='navbar'>
-        <h2>
-        <Link to='/'>MoviesLib</Link>
-        </h2>
-        <Link to='/movie/1'>Movie</Link>
-        <Link to='/search'>Search</Link>
-      </nav>
-      <h2>MoviesLib</h2>
+      <Navbar />
       <Outlet />
     </div>
   )
